da94fd9880b24da315c077cbf95af3ae2cfb9785
MasonJarCat <MasonMcClinticC@gmail.com>
2022-11-12 16:12:50 -0500

    new tutorial start

--- Bot/bot.py
+++ Bot/bot.py
@@ -4,5 +4,21 @@ import os
 import discord
 from dotenv import load_dotenv
 
-load_dotenv
-TOKEN = os.getenv("MTAzMjY4MDgzNTY2MjQ3OTQyMA.GFCbpM.kRir2atENnQDzF4IXdU_olYCWI8WbBXXRUdbyQ")
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+client.run(TOKEN)
