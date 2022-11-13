87eab9e7aebf44d3c850bdf6cdad52826b0b6206
MasonJarCat <MasonMcClinticC@gmail.com>
2022-11-12 19:44:13 -0500

    yo

--- Bot/bot.py
+++ Bot/bot.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-
-client = discord.Client()
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
