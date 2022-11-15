ca0b53ba876bcfa090b8d725f643904ffb8f37be
MasonJarCat <MasonMcClinticC@gmail.com>
2022-11-15 18:57:03 -0500

    Update bot.py

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,15 +1,20 @@
 # bot.py
 import os
-
+from discord.ext import commands
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+#intents for messaging and defualts
 intents = discord.Intents.default()
 intents.message_content = True
+
+#passing the intents to the client and creating it
 client = discord.Client(intents=intents)
+#bot creation
+bot = commands.Bot(intents=intents,command_prefix='$')
 
 @client.event
 async def on_ready():
@@ -25,5 +30,8 @@ async def on_ready():
 async def on_message(message):
         if(message.content.startswith("!rngU")):
             await message.channel.send('Hello!')
-
+@bot.command()
+async def rngU(ctx, arg: int):
+        await ctx.send(arg)
+    
 client.run(TOKEN)
