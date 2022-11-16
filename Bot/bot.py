67c3bec5c83b8c8d4d7deb138234d143dc3f9a2b
msmoldavchuk <Michaelm4082@gmail.com>
2022-11-16 17:40:46 -0500

    Update bot.py

--- Bot/bot.py
+++ Bot/bot.py
@@ -5,7 +5,7 @@ import discord
 from dotenv import load_dotenv
 import numpy as np
 import pandas as pd
-import matplotlib as mp
+import matplotlib as plt
 import datetime as dt
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -15,8 +15,10 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 #passing the intents to the client and creating it
-
+#creates dataframe
+df1 = pd.DataFrame(columns=("Rating","Time", "Day"))  
 #bot creation
+specialOfTheDay = []
 bot = commands.Bot(command_prefix='!',intents=intents)
 @bot.event
 async def on_ready():
@@ -26,9 +28,46 @@ async def on_ready():
 @bot.command()
 async def rngU(ctx, arg: int):
         await ctx.send(arg)
+#populates array
 @bot.command()
 async def rngH(ctx, arg: int):
         await ctx.send(arg)
-        await ctx.send(ctx.message.created_at)
-    
+
+        dti = pd.to_datetime([ctx.message.created_at])
+        dti = dti.tz_convert("US/Eastern")
+        df1.loc[len(df1.index)] = [arg, dti.time, dti.day_name()] 
+       # await ctx.send(df1)
+       # print(df1)
+
+#gets average
+@bot.command()
+async def getRate(ctx):
+    var = 0
+    for x in range(len(df1.index)):
+        var = var + df1.loc[x,"Rating"]
+    var = var / len(df1.index)
+    await ctx.send("Average Hans Rating Today:"+ var)
+
+  #report the days special  
+@bot.command()
+async def repSpec(ctx, arg):
+    specialOfTheDay.append(arg)
+    await ctx.send("Thank you for reporting")
+
+# get the days special
+@bot.command()
+async def spec(ctx):
+    str = "The Specials Of The Day Are: "
+    for i in range(len(specialOfTheDay)):
+        str = " ".join([str, specialOfTheDay[i]])
+    await ctx.send(str)
+
+#broken do NOT use
+@bot.command()
+async def getGGraph(ctx):
+   # df1.plot.scatter(x="Rating", y="Time", alpha=0.5)
+    df1["Rating"].plot()
+    plt.show()
+    await ctx.send(plt.show())
+
 bot.run(TOKEN)
