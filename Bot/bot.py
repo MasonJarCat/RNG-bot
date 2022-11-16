c42343972ec2a7127f38ed2fedec8ef2932e6740
MasonJarCat <MasonMcClinticC@gmail.com>
2022-11-16 15:25:00 -0500

    Update bot.py

--- Bot/bot.py
+++ Bot/bot.py
@@ -16,26 +16,13 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 #passing the intents to the client and creating it
-client = discord.Client(intents=intents)
+
 #bot creation
 bot = commands.Bot(intents=intents,command_prefix='$')
 #creates dataframe
 df1 = pd.DataFrame(columns=("Rating","Time", "Day"))  
 
-@client.event
-async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
-@client.event
-async def on_message(message):
-        if(message.content.startswith("!rngU")):
-            await message.channel.send('Hello!')
 
 @bot.command()
 async def rngU(ctx, arg: int):
@@ -43,4 +30,4 @@ async def rngU(ctx, arg: int):
         df1.loc[len(df1.index)] = [arg, "12:20", "Monday"]
 
 
-client.run(TOKEN)
+bot.run(TOKEN)
