832846bed670fbf733701c335d2bf01c5f721c0c
msmoldavchuk <Michaelm4082@gmail.com>
2022-11-16 15:18:59 -0500

    Update bot.py

--- Bot/bot.py
+++ Bot/bot.py
@@ -3,6 +3,10 @@ import os
 from discord.ext import commands
 import discord
 from dotenv import load_dotenv
+import numpy as np
+import pandas as pd
+import matplotlib as mp
+import datetime as dt
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -15,6 +19,8 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 #bot creation
 bot = commands.Bot(intents=intents,command_prefix='$')
+#creates dataframe
+df1 = pd.DataFrame(columns=("Rating","Time", "Day"))  
 
 @client.event
 async def on_ready():
@@ -33,5 +39,11 @@ async def on_message(message):
 @bot.command()
 async def rngU(ctx, arg: int):
         await ctx.send(arg)
-    
+
+@bot.command()
+async def rngU(ctx, arg: int):
+        await ctx.send(arg)
+        df1.loc[len(df1.index)] = [10, "12:20", "Monday"]
+
+
 client.run(TOKEN)
