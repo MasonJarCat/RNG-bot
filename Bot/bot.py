b556c89287837d57c4e265ffcfb3889b72cf7e75
MasonJarCat <MasonMcClinticC@gmail.com>
2022-11-15 20:08:50 -0500

    Update bot.py

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,7 +1,7 @@
 # bot.py
 import os
-from discord.ext import commands
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,26 +12,20 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 #passing the intents to the client and creating it
-client = discord.Client(intents=intents)
+
 #bot creation
-bot = commands.Bot(intents=intents,command_prefix='$')
-
-@client.event
+bot = commands.Bot(command_prefix='!',intents=intents)
+@bot.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
-@client.event
-async def on_message(message):
-        if(message.content.startswith("!rngU")):
-            await message.channel.send('Hello!')
 @bot.command()
 async def rngU(ctx, arg: int):
         await ctx.send(arg)
+@bot.command()
+async def rngH(ctx, arg: int):
+        await ctx.send(arg)
+        await ctx.send(ctx.message.created_at)
     
-client.run(TOKEN)
+bot.run(TOKEN)
